201cd67cf4c277cffd4d6a3eb7a38a23c3f4faef
David Brito <39559632+davbrito@users.noreply.github.com>
2026-03-15 22:07:06 -0400

    Refactor AppEnv interface to use Env type for Bindings

--- worker/config.ts
+++ worker/config.ts
@@ -1,12 +1,3 @@
 export interface AppEnv {
-  Bindings: {
-    OLLAMA_URL?: string;
-    OLLAMA_API_KEY: string;
-    OMDB_API_KEY: string;
-    ELEVENLABS_API_KEY?: string;
-    ELEVENLABS_VOICE_ID?: string;
-    TURNSTILE_SECRET_KEY?: string;
-    JWT_SECRET_KEY?: string;
-    SESSION_JWT_TTL_SECONDS?: string;
-  };
+  Bindings: Env;
 }
